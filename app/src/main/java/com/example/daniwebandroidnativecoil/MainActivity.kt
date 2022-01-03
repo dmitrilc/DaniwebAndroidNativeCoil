54548bf913d51f83712537130657c1deb065de8a
Dimitri Nguyen <dmitrilc@users.noreply.github.com>
2022-01-03 18:39:56 -0500

    Add Urls and Internet Permission

--- app/src/main/java/com/example/daniwebandroidnativecoil/MainActivity.kt
+++ app/src/main/java/com/example/daniwebandroidnativecoil/MainActivity.kt
@@ -10,6 +10,7 @@ import coil.imageLoader
 import coil.load
 import coil.util.CoilUtils
 import okhttp3.OkHttpClient
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +21,11 @@ class MainActivity : AppCompatActivity() {
         val imageView = findViewById<ImageView>(R.id.imageView)
 
         button.setOnClickListener {
-            imageView.load("https://google.com")
+            when(Random.nextInt(0, 3)){
+                0 -> imageView.load(getString(R.string.rect))
+                1 -> imageView.load(getString(R.string.triangle))
+                2 -> imageView.load(getString(R.string.circle))
+            }
         }
     }
 
